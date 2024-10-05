9c0a92a5ac4ad909ff9e875e2cad0cb06be00efd
migle-kirilovaite <migle.kirilovaite@gmail.com>
2024-10-05 21:39:36 +0300

    Wrote load_data

---
+++ main.py
@@ -0,0 +1,9 @@
+﻿from src.load_data import load_all_fastas
+
+def main():
+    directory = "data"
+    sequences = load_all_fastas(directory)
+    print(sequences)
+
+if __name__ == '__main__':
+    main()
