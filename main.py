7a9a1b412d51a5334d6e57ee6824076ff872cbba
migle-kirilovaite <migle.kirilovaite@gmail.com>
2024-10-05 22:05:17 +0300

    Minor clarity changes

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-﻿from src.load_data import load_all_fastas
+﻿from src.load_data import load_all_viruses
 
 def main():
     directory = "data"
-    mamalian_sequences, bacterial_sequences = load_all_fastas(directory)
+    mamalian_sequences, bacterial_sequences = load_all_viruses(directory)
     print(mamalian_sequences)
     print(bacterial_sequences)
 
--- src/load_data.py
+++ src/load_data.py
@@ -5,25 +5,24 @@ from Bio import SeqIO
 def load_fasta(file_path):
     sequences = []
     for record in SeqIO.parse(file_path, "fasta"):
-        sequences.append((record.id, str(record.seq)))  # Store ID and sequence
+        sequences.append((record.id, str(record.seq)))
     return sequences
 
 
-def load_all_fastas(directory):
-    mammalian_sequences = {}
-    bacterial_sequences = {}
+def load_all_viruses(directory):
+    mammalian_viruses = {}
+    bacterial_viruses = {}
 
     for filename in os.listdir(directory):
         if filename.endswith(".fasta"):
             file_path = os.path.join(directory, filename)
-            sequences = load_fasta(file_path)
+            viruses = load_fasta(file_path)
 
             if filename.startswith("mamalian"):
-                for record_id, sequence in sequences:
-                    mammalian_sequences[record_id] = sequence
+                mammalian_viruses.update(viruses)
             elif filename.startswith("bacterial"):
-                for record_id, sequence in sequences:
-                    bacterial_sequences[record_id] = sequence
+                bacterial_viruses.update(viruses)
 
-    return mammalian_sequences, bacterial_sequences
+    return mammalian_viruses, bacterial_viruses
+
 
