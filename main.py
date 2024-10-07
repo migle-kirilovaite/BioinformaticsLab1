e83a5c912e4fad27654604503f3f3839f0e6a53e
migle-kirilovaite <migle.kirilovaite@gmail.com>
2024-10-07 13:42:09 +0300

    Added find regions file with functions

--- main.py
+++ main.py
@@ -1,10 +1,26 @@
 ﻿from src.load_data import load_all_viruses
+from src.find_regions import find_coding_regions, reverse_complement, filter_short_regions
 
 def main():
-    directory = "data"
-    mamalian_sequences, bacterial_sequences = load_all_viruses(directory)
-    print(mamalian_sequences)
-    print(bacterial_sequences)
+    directory = "data/"
+    mammalian_sequences, bacterial_sequences = load_all_viruses(directory)
+
+    print("Processing Mammalian Sequences:")
+    for record_id, sequence in mammalian_sequences.items():
+        regions_forward = find_coding_regions(sequence)
+        regions_reverse = find_coding_regions(reverse_complement(sequence))
+
+        all_regions = regions_forward + regions_reverse
+        filtered_regions = filter_short_regions(all_regions)
+
+
+    print("\nProcessing Bacterial Sequences:")
+    for record_id, sequence in bacterial_sequences.items():
+        regions_forward = find_coding_regions(sequence)
+        regions_reverse = find_coding_regions(reverse_complement(sequence))
+
+        all_regions = regions_forward + regions_reverse
+        filtered_regions = filter_short_regions(all_regions)
 
 if __name__ == '__main__':
     main()
---
+++ src/find_regions.py
@@ -0,0 +1,20 @@
+﻿def find_coding_regions(sequence):
+    start_codon = "ATG"
+    stop_codons = ["TAA", "TAG", "TGA"]
+    regions = []
+
+    for i in range(len(sequence) - 2):
+        if sequence[i:i+3] == start_codon:
+            for j in range(i + 3, len(sequence) - 2, 3):
+                codon = sequence[j:j+3]
+                if codon in stop_codons:
+                    regions.append((i, j + 3))
+                    break
+    return regions
+
+def reverse_complement(sequence):
+    complement = {'A': 'T', 'T': 'A', 'C': 'G', 'G': 'C'}
+    return ''.join([complement[base] for base in reversed(sequence)])
+
+def filter_short_regions(regions, min_length=100):
+    return [(start, stop) for start, stop in regions if stop - start >= min_length]
